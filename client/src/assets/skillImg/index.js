25575b4d03944475fbd652ad4683c6abd8e0efe3
Saurabh-1SP <113672743+Saurabh-1SP@users.noreply.github.com>
2023-04-25 23:54:12 +0530

    disconnecting from server and small css changes

---
+++ client/src/assets/skillImg/index.js
@@ -0,0 +1,25 @@
+import express from './express.svg'
+import html from './html.svg'
+import css from './css.svg'
+import javascript from './javascript.svg'
+import mongodb from './mongodb.svg'
+import mui from './mui.svg'
+import nodejs from './nodejs.svg'
+import react from './react.svg'
+import redux from './redux.svg'
+import sass from './sass.svg'
+import typescript from './typescriptlang.svg'
+
+export {
+    express,
+    html,
+    javascript,
+    mongodb,
+    mui,
+    nodejs,
+    react,
+    redux,
+    sass,
+    typescript,
+    css
+}
