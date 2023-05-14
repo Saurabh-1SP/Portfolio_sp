6408d07e7bf4ba28d43487063abbdcd5763e7cae
Saurabh-1SP <113672743+Saurabh-1SP@users.noreply.github.com>
2023-05-14 21:49:59 +0530

    Some CSS improvments

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -2,10 +2,9 @@ import email from '../assets/email.png';
 import mobile from '../assets/mobile.png';
 import api from '../assets/api.png';
 
-import about01 from '../assets/about01.png';
+import about01 from '../assets/about01.jpeg';
 import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
-import about04 from '../assets/about04.png';
+import about03 from '../assets/about03.jpg';
 
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
@@ -24,7 +23,6 @@ export default {
   about01,
   about02,
   about03,
-  about04,
   profile,
   circle,
   preview,
--- client/src/container/About/About.jsx
+++ client/src/container/About/About.jsx
@@ -13,24 +13,23 @@ const About = () => {
         {
             tittle: 'Backend Developer',
             description: ` I'm a backend developer with a passion for solving complex problems and delivering effective solutions. I have experience working with technologies such as Node.js, Express, and MongoDB, and I excel at building scalable, secure, and efficient server-side applications. I have a strong understanding of server-side scripting, and I'm always seeking to improve my skills and stay current with the latest industry advancements. Whether working on a simple backend service or a complex system, I bring my best to every project and strive to exceed expectations.`,
-            imgUrl: images.about01
+            imgUrl: images.about02
         },
         {
             tittle: 'React Developer',
             description: `I am a React developer with a passion for crafting user-friendly web experiences. My expertise in JavaScript, HTML, and CSS allows me to turn ideas into polished, functional interfaces. I'm constantly seeking new ways to push the limits of what's possible, always striving to learn and grow as a developer. Whether working on a simple site or a complex application, I bring my best to every project and always aim to deliver outstanding results.`,
-            imgUrl: images.about03
+            imgUrl: images.about01
         },
         {
             tittle: 'Frontend Developer',
             description: ` I'm a frontend developer with a focus on React. I have a passion for creating beautiful and intuitive user interfaces that deliver a great experience. With a strong foundation in JavaScript, HTML, and CSS, I bring ideas to life and make sure that they work seamlessly for users. I'm always looking for ways to improve my skills and stay up-to-date with the latest trends in frontend development. Whether working on a small project or a complex application, I bring my best to every task and strive to exceed expectations.`,
-            imgUrl: images.about04
+            imgUrl: images.about03
         },
     ]
 
   return (
     <div id='About' className='app__about'>
       <h2 className='head-text'>I Know that <span>Good Development</span><br/>means <span>Good Business</span></h2>
-      
       <div className='app__profiles'>
         {abouts.map((about, index)=>(
           <motion.div
