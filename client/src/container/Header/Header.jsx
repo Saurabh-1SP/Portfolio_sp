07fa790f5cc3afb5c8dd1b3840eb5b58e8f93ce5
Saurabh-1SP <113672743+Saurabh-1SP@users.noreply.github.com>
2023-05-23 14:29:28 +0530

    Some Minor Changes

--- client/src/container/Header/Header.jsx
+++ client/src/container/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import './Header.scss'
-
-import { AppWraper } from '../../Wrappers'
 import {motion} from 'framer-motion'
+// import Moveable from 'react-moveable'
+
+import './Header.scss'
+import { AppWraper } from '../../Wrappers'
 import {images} from '../../constants'
 
 
@@ -18,40 +19,41 @@ import {images} from '../../constants'
 // }
 
 const Header = () => {
+
   return (
     <div id='home' className='app__header app__flex'>
-<div className="area" >
-            <ul className="circles">
-              <img src={images.circle} alt="" />
-              <img src={images.circle} alt="" />
-              <img src={images.circle} alt="" />
-              <img src={images.circle} alt="" />
-              <img src={images.circle} alt="" />
-              <img src={images.circle} alt="" />
-              <img src={images.circle} alt="" />
-              <img src={images.circle} alt="" />
-              <img src={images.circle} alt="" />
-              <img src={images.circle} alt="" />
-            </ul>
-      <motion.div
-      whileInView={{x:[-100,0],opacity:[0,1]}}
-      transition={{duration:0.5}}
-      className='app__header-info'
-      >
-        <div className='app__header-badge'>
-          <div className='badge-cmp app_flex'>
-            <span>👋</span>
-            <div style={{marginLeft: 20}}>
-              <p className='p-text'>Hello, I am</p>
-              <h1 className='head-text'>Saurabh</h1>
+      <div className="area" >
+        <ul className="circles">
+          <img src={images.circle} alt="" />
+          <img src={images.circle} alt="" />
+          <img src={images.circle} alt="" />
+          <img src={images.circle} alt="" />
+          <img src={images.circle} alt="" />
+          <img src={images.circle} alt="" />
+          <img src={images.circle} alt="" />
+          <img src={images.circle} alt="" />
+          <img src={images.circle} alt="" />
+          <img src={images.circle} alt="" />
+        </ul>
+        <motion.div
+        whileInView={{x:[-100,0],opacity:[0,1]}}
+        transition={{duration:0.5}}
+        className='app__header-info'
+        >
+          <div className='app__header-badge'>
+            <div className='badge-cmp app_flex'>
+              <span>👋</span>
+              <div style={{marginLeft: 20}}>
+                <p className='p-text'>Hello, I am</p>
+                <h1 className='head-text'>Saurabh</h1>
+              </div>
+            </div>
+            <div className="tag-cmp app__flex">
+              <p className='p-text'>Web Developer</p>
+              <p className='p-text'>Freelancer</p>
             </div>
           </div>
-          <div className="tag-cmp app__flex">
-            <p className='p-text'>Web Developer</p>
-            <p className='p-text'>Freelancer</p>
-          </div>
-        </div>
-      </motion.div>
+        </motion.div>
       </div>
       {/* <motion.div 
       whileInView={{opacity:[0,1]}}
